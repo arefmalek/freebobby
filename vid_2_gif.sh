a9d7d1ad6c3fc8dae2b14f600f5df9eb77407f18
Aref Malek <arefmalek02@gmail.com>
2021-01-17 13:55:55 -0800

    FINALLY GOT THE GIFS

---
+++ vid_2_gif.sh
@@ -0,0 +1,5 @@
+ffmpeg -i input.mkv -ss 00:00 -t 00:14 -gifflags -transdiff ~/Desktop/freebobby/part1.gif
+ffmpeg -i input.mkv -ss 00:23 -t 00:30 -gifflags -transdiff ~/Desktop/freebobby/part2.gif
+ffmpeg -i input.mkv -ss 00:34 -t 00:39 -gifflags -transdiff ~/Desktop/freebobby/part3.gif
+ffmpeg -i input.mkv -ss 01:00 -t 01:03 -gifflags -transdiff ~/Desktop/freebobby/part4.gif
+ffmpeg -i input.mkv -ss 01:03 -t 01:22 -gifflags -transdiff ~/Desktop/freebobby/part5.gif
